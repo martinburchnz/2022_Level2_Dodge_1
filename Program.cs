a96a027af49dec112f2d2b323a25ec5420079f80
Martin Burch <martinb@mmc.school.nz>
2022-05-11 08:17:14 +1200

    Planets moving down the game panel

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace _2022_Level2_Dodge_1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmDodge());
         }
     }
 }
