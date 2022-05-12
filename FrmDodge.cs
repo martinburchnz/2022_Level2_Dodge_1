3039c45a5cb015fd68b22417d8a2a26dda527420
Martin Burch <martinb@mmc.school.nz>
2022-05-12 12:59:52 +1200

    Move Spaceship left and right  checking for collisions

--- FrmDodge.cs
+++ FrmDodge.cs
@@ -18,6 +18,7 @@ namespace _2022_Level2_Dodge_1
         Random yspeed = new Random();
         Spaceship spaceship = new Spaceship();
         bool left, right;
+        int score, lives;
         string move;
 
 
@@ -81,17 +82,47 @@ namespace _2022_Level2_Dodge_1
 
         }
 
+        private void FrmDodge_Load(object sender, EventArgs e)
+        {
+            // pass lives from LblLives Text property to lives variable
+            lives = int.Parse(LblLives.Text);
+
+        }
+
+        private void CheckLives()
+        {
+            if (lives == 0)
+            {
+                TmrPlanet.Enabled = false;
+                TmrShip.Enabled = false;
+                MessageBox.Show("Game Over");
+
+            }
+        }
+
         private void TmrPlanet_Tick(object sender, EventArgs e)
         {
             for (int i = 0; i < 7; i++)
             {
                 planet[i].MovePlanet();
+                if (spaceship.spaceRec.IntersectsWith(planet[i].planetRec))
+                {
+                    //reset planet[i] back to top of panel
+                    planet[i].y = 30; // set  y value of planetRec
+                    lives -= 1;// lose a life
+                    LblLives.Text = lives.ToString();// display number of lives
+                    CheckLives();
+                }
                 //if a planet reaches the bottom of the Game Area reposition it at the top
                 if (planet[i].y >= PnlGame.Height)
                 {
-                    planet[i].y = 30;
-                }
+                    score += 1;//update the score
+                    LblScore.Text = score.ToString();// display score
 
+                    planet[i].y = 30;
+           
+                }
+               
             }
             PnlGame.Invalidate();//makes the paint event fire to redraw the panel
 
